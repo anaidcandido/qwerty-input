662789345e3bd0e99cbd5dc314a2ca0027005065
Anaid Candido <anaid.candido@gmail.com>
2022-07-25 16:52:23 +0100

    add message hasSpecialCharacter

--- src/components/WordDescription.tsx
+++ src/components/WordDescription.tsx
@@ -8,12 +8,14 @@ function WordDescription({ wordToDescribe }: WordDescriptionProps): JSX.Element 
   const hasAtLeastFiveCharacters = wordToDescribe.length > 5;
   const containsAVowel = hasAVowel(wordToDescribe);
   const hasTheLetterQ = wordToDescribe.toLowerCase().match('q');
+  const hasSpecialCharacter = wordToDescribe[wordToDescribe.length - 1] 
 
   return (
     <ul>
       {hasAtLeastFiveCharacters && <li>Has at least five characters</li>}
       {containsAVowel && <li>Contains a vowel</li>}
       {hasTheLetterQ && <li>Contains the letter Q</li>}
+      {hasSpecialCharacter === '!' && <li>Contains a special character</li>}
     </ul>
   )
 }
